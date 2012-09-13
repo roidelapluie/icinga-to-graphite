474fe9cef9ee41593e70e4849a112fe5b4579d6c
Julien Pivotto <roidelapluie@inuits.eu>
2012-09-13 14:10:25 +0200

    add full path to the wak script

--- transform_perfdata.sh
+++ transform_perfdata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 carbon_host=10.0.1.42
 carbon_port=2003
+awk_sript="/usr/local/lib/transform_perfdata.awk"
 
 tmpfile="$(mktemp)"
-mv "$1" "$tmpfile"
-awk -f transform_perfdata.awk < "$tmpfile" > /dev/tcp/${graphite_host}/${graphite_port}
-rm "$tmpfile"
+mv "$1" "${tmpfile}"
+awk -f "${awk_script}"< "$tmpfile" > /dev/tcp/${graphite_host}/${graphite_port}
+rm "${tmpfile}"
